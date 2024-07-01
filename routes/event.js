4febd0260a3ab26fc9b0a31867c3453201b84c6e
japhlett <jatakyiwaa001@st.ug.edu.gh>
2024-07-01 16:14:46 +0000

    Add event route

--- routes/event.js
+++ routes/event.js
@@ -1,2 +1,16 @@
 import { Router } from "express";
+import { getEvents,getEvent,deleteEvent,postEvent,patchEvent } from "../controllers/event.js";
+
+// create a router
+const eventRouter = Router();
+
+// define routes
+eventRouter.get('/events',getEvents);
+eventRouter.get('/events/:id',getEvent);
+eventRouter.post('/events',postEvent);
+eventRouter.patch('/events/:id',patchEvent);
+eventRouter.delete('/events/:id',deleteEvent);
+
+// exporting the events router
+export default eventRouter;
 
