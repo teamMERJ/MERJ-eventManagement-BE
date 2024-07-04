8a18c2a878d02ba914aae84d872bf808d3d6609d
japhlett <jatakyiwaa001@st.ug.edu.gh>
2024-07-04 12:13:17 +0000

    Allow queries based on various fields

--- controllers/event.js
+++ controllers/event.js
@@ -1,66 +1,79 @@
-import {EventModel} from "../models/event.js";
+import { EventModel } from "../models/event.js";
 
-// getting all events 
-export const getEvents = async(req,res,next) =>{
+// getting all events
+ export const getOneEvent = async(req,res,next) =>{
     try {
-        // Get events from database
-        const allEvents = await EventModel.find();
-        // Return all events as response
-        res.json(allEvents);
+        // Get one event from database
+        const oneEvent = await EventModel.findById(  req.params.id);
+        // Return one events as response
+        res.json(oneEvent);
     } catch (error) {
        next(error);
     }
 }
 
 // getting one event
-export const getEvent = async(req,res,next) =>{
-    try {
-        // Get an event from database
-        const event = await EventModel.findById(req.params.id);
-        // Return an event with an id as a response
-        res.json(event);
-    } catch (error) {
-        next(error);
-    }
-}
+export const getEvent = async (req, res, next) => {
+  try {
+    // Get query params
+    const {
+      limit = 4,
+      skip = 0,
+      filter = "{}",
+      sort = "{}",
+      fields = "{}",
+    } = req.query;
+    // Get all categories from database
+    const event = await EventModel.find(JSON.parse(filter))
+      .select(JSON.parse(fields))
+      .limit(JSON.parse(skip))
+      .sort(JSON.parse(sort));
+    // Return response
+    res.status(200).json(event);
+  } catch (error) {
+    next(error);
+  }
+};
 
 // Adding a new event
-export const postEvent = async(req,res,next) =>{
-    try {
-        // posting an event to the database
-        const newEvent = await EventModel.create({
-            ...req.body,
-            flier: req.file.filename
-        });
-        // return response
-        res.status(201).json(newEvent);
-    } catch (error) {
-        next(error);
-    }
-}
+export const postEvent = async (req, res, next) => {
+  try {
+    // posting an event to the database
+    const newEvent = await EventModel.create({
+      ...req.body,
+      flier: req.file.filename,
+    });
+    // return response
+    res.status(201).json(newEvent);
+  } catch (error) {
+    next(error);
+  }
+};
 
 // Updating an event
-export const patchEvent = async(req,res, next) =>{
-    try {
-        const Price = req.body.price
-        console.log('request', Price)
-        // updating details of an event
-        const updatedEvent = await EventModel.findByIdAndUpdate(req.params.id, {price: Price});
-        // return response
-        res.json(updatedEvent);
-    } catch (error) {
-        next(error)
-    }
-}
+export const patchEvent = async (req, res, next) => {
+  try {
+    // updating details of an event
+    const updatedEvent = await EventModel.findByIdAndUpdate(
+      req.params.id,
+      { ...req.body, flier: req?.file?.filename },
+      { new: true }
+    );
+    // return response
+    res.status(200).json(updatedEvent);
+  } catch (error) {
+    next(error);
+  }
+};
 
 // Deleting an event
-export const deleteEvent = async(req,res,next) =>{
-    try {
-        // deleting an event
-        const deletedEvent = await EventModel.findByIdAndDelete(req.params.id);
-        // return response
-        res.json(`Event with ID ${req.params.id} deleted`);
-    } catch (error) {
-        next(error)
-    }
-}
+export const deleteEvent = async (req, res, next) => {
+  try {
+    // deleting an event
+    const deletedEvent = await EventModel.findByIdAndDelete(req.params.id);
+    // return response
+    res.json(`Event with ID ${req.params.id} deleted`);
+  } catch (error) {
+    next(error);
+  }
+};
